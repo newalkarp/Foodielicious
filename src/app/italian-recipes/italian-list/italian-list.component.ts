9bbfe6f7cd9446b645dfdf2059d5be4f20e77ff8
Priyal Newalkar <34749249+newalkarp@users.noreply.github.com>
2019-10-23 16:34:38 -0500

    Update italian-list.component.ts

--- src/app/italian-recipes/italian-list/italian-list.component.ts
+++ src/app/italian-recipes/italian-list/italian-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/recipes.model';
 
 @Component({
   selector: 'app-italian-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./italian-list.component.css']
 })
 export class ItalianListComponent implements OnInit {
+  italianRecipes: Recipe[] = [
+    new Recipe('Chicken Penne Alfredo','Simply a test',"https://upload.wikimedia.org/wikipedia/commons/b/bc/Pasta_with_a_Chicken_Lemon_Cream_Sauce_%28764644143%29.jpg")
+  ];
 
   constructor() { }
 
